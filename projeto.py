f19a2d4ecb742fcb04668282dc35a02fa3418aa3
Felipe <felipe21fab@gmail.com>
2022-11-12 18:07:50 -0300

    Created thirdRestriction

--- projeto.py
+++ projeto.py
@@ -14,6 +14,10 @@ with open('./pacientes/column_bin_5a_3p.csv', mode='r') as arquivo_csv:
     #Pegando os pacientes
 pd = pd.read_csv('./pacientes/column_bin_5a_3p.csv')
 patientStatus = pd['P'].values.tolist()
+
+patientsNoDisease = pd.loc[pd['P'] == 0]
+'''print(patientsNoDisease['PI <= 42.09'].values)'''
+
 # atomica do tipo Xatributo,regra_atual,regra_aparece_na_formula
 m = 4
 rules = ['gt', 'le', 's']
@@ -74,10 +78,9 @@ def secondRestriction(attributes, m):
         formula_list.clear()
     return and_all(formulas)
 
-print(secondRestriction(attributes, m))
 
 
-def thirdRestriction(attribute, m):
+def thirdRestriction(attributes, m):
     '''Para cada paciente sem patologia e cada regra, algum atributo do paciente não pode ser aplicado à
 regra.'''
     '''formula_list = []
@@ -85,8 +88,44 @@ regra.'''
     for i in range(1,m+1):
         for j in attributes:
 '''
+    patients = len(patientStatus) - numberOfPatientsWithDisease(patientStatus)
+    formula_list = []
+    formulas = []
+    for i in range(1, m+1):
+            for j in attributes:
+                if patientsNoDisease[j].values == 1: 
+                    formula =  Atom('x' + str(j) + '_' + str(i) + '_' + 'gt')
+                    formula_list.append(formula)
+                else:
+                    formula =  Atom('x' + str(j) + '_' + str(i) + '_' + 'le')
+                    formula_list.append(formula)
+            formulas.append(or_all(formula_list))
+            formula_list.clear()
+    return and_all(formulas)
 
-def fifthRestriction(attributes, m):
+print(thirdRestriction(attributes,m))
+
+
+
+
+def fourthRestriction(attributes, m):
+    '''Para cada paciente com patologia, cada regra e cada atributo, se o atributo do paciente não se aplicar
+ao da regra, então a regra não cobre esse paciente.'''
+    patients = numberOfPatientsWithDisease(patientStatus)
+    formula_list = []
+    formulas = []
+    for i in range(1, m+1):
+        for k in range(5):
+            for p in range(1, patients+1):
+                for j in attributes:
+                    formula = Implies(Atom('x' + str(j) + '_'+ str(i) + '_' + str(k)),Not(Atom('c' + str(i) + '_' + str(p))))
+                    formula_list.append(formula)
+        formulas.append(and_all(formula_list))
+        formula_list.clear()
+    return and_all(formulas)
+
+
+def fifthRestriction(m):
     '''Cada paciente com patologia deve ser coberto por alguma das regras.'''
     formula_list = []
     formulas = []
@@ -99,4 +138,3 @@ def fifthRestriction(attributes, m):
         formula_list.clear()
     return and_all(formulas)
         
-print(fifthRestriction(attributes, m))
